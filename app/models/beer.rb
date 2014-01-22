a10da0157a076162a7cf38f48a54d5c803080ab6
Marko Haanranta <marko.haanranta@helsinki.fi>
2014-01-22 13:35:35 +0200

    exercise 13 done

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,6 +1,6 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
-  has_many :ratings
+  has_many :ratings, :dependent => :destroy
 
   def average_rating
   "#{(ratings.inject(0.0) { |sum ,rating |sum+rating.score }/ratings.count).round(1)}"
